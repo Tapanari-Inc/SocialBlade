9c9d1a13be73bb9ddb41809922783e6a4c07deb7
BobbyRackov <bobisvetoslavovrachkov@hotmail.com>
2016-12-11 19:38:53 +0200

    Some Refactoring Done++

---
+++ src/SocialBladeBundle/Repository/UserRepository.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace SocialBladeBundle\Repository;
+
+
+class UserRepository extends \Doctrine\ORM\EntityRepository
+{
+}
---
+++ src/SocialBladeBundle/SocialBladeBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SocialBladeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SocialBladeBundle extends Bundle
+{
+}
